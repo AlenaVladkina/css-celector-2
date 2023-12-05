655cc8e6123b3e1523b0b83810c25900193828f2
Alona Rudyk <rudik.alena1011@gmail.com>
2023-12-05 08:38:13 +0200

    sass

--- src/route/index.js
+++ src/route/index.js
@@ -149,5 +149,15 @@ router.get('/template-3', function (req, res) {
 })
 // ================================================================
 
-// Підключаємо роутер до бек-енду
-module.exports = router
+router.get('/dashboard', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('dashboard', {
+    layout: null,
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+//===================================================================
+
