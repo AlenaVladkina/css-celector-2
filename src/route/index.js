d78bed8a13873564ccd16babb91faf2158eae16b
Alona Rudyk <rudik.alena1011@gmail.com>
2023-12-06 21:05:50 +0200

    dashboard-fix2

--- src/route/index.js
+++ src/route/index.js
@@ -16,6 +16,17 @@ router.get('/', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+//===================================================
+router.get('/dashboard', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('dashboard', {
+    layout: null,
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
